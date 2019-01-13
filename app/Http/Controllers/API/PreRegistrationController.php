458e55491afc5636ee63ac59e0258ab37a3b7936
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2019-01-13 11:32:08 +0100

    Create PreRegistrationController and register routes to it

---
+++ app/Http/Controllers/API/PreRegistrationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\PreRegistration;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PreRegistrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PreRegistration  $preRegistration
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PreRegistration $preRegistration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PreRegistration  $preRegistration
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PreRegistration $preRegistration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PreRegistration  $preRegistration
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PreRegistration $preRegistration)
+    {
+        //
+    }
+}
