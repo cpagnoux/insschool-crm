5c0cada1bf1255703408b91a9f1b83c5561232c9
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2019-01-13 11:37:12 +0100

    Define pre_registrations schema

--- database/migrations/2019_01_13_101017_create_pre_registrations_table.php
+++ database/migrations/2019_01_13_101017_create_pre_registrations_table.php
@@ -15,6 +15,28 @@ class CreatePreRegistrationsTable extends Migration
     {
         Schema::create('pre_registrations', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('mobile')->nullable();
+            $table->string('mobile_father')->nullable();
+            $table->string('mobile_mother')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->boolean('with_lessons');
+            $table->enum('plan', [
+                'ANNUAL',
+                'QUARTERLY',
+            ])->nullable();
+            $table->enum('means_of_knowledge', [
+                'ADVERTISING_PANEL',
+                'INTERNET',
+                'POSTER_FLYER',
+                'WORD_OF_MOUTH',
+            ]);
             $table->timestamps();
         });
     }
