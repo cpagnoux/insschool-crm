017bda3fcf666b66be660aebace1754677f35703
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2019-02-09 12:21:51 +0100

    Define contacts schema

--- database/migrations/2019_02_09_105126_create_contacts_table.php
+++ database/migrations/2019_02_09_105126_create_contacts_table.php
@@ -15,6 +15,23 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('mobile')->nullable();
+            $table->string('mobile_father')->nullable();
+            $table->string('mobile_mother')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->enum('means_of_knowledge', [
+                'ADVERTISING_PANEL',
+                'INTERNET',
+                'POSTER_FLYER',
+                'WORD_OF_MOUTH',
+            ]);
             $table->timestamps();
         });
     }
